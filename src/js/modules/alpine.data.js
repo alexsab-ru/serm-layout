eb91ba8c4eba5e216b88c23f88c6240dea3516a1
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-12-09 08:30:26 +0500

    social component

--- src/js/modules/alpine.data.js
+++ src/js/modules/alpine.data.js
@@ -1,21 +1,8 @@
 import Alpine from 'alpinejs'
  
+
+
+document.addEventListener('alpine:init', () => {})
+
 // window.Alpine = Alpine
-
-import icons from './icons';
-
-document.addEventListener('alpine:init', () => {
-	Alpine.data('social', () => ({
-        icons: {
-            vk: icons.vk,
-            odnk: icons.odnk,
-            fb: icons.fb,
-            inst: icons.inst,
-            youtube: icons.youtube,
-            tiktok: icons.tiktok,
-            telegram: icons.telegram,
-        }
-    }))
-})
- 
 // Alpine.start()
